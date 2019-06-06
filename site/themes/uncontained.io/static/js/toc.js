b343c86dcc5e6bc0578fa9984a2896dc130e2bf4
Eric Sauer <esauer@redhat.com>
2019-06-06 09:49:01 -0400

    Added scrollspy

--- site/themes/uncontained.io/static/js/toc.js
+++ site/themes/uncontained.io/static/js/toc.js
@@ -1,4 +1,11 @@
 // This is a hack to move the asciidoctor generated Table of Contents.
 $(document).ready(function() {
+  // Move table of contents to the sidebar
   $('#toc_parent').append($('#toc'));
+
+  // Enable bootstrap scrollspy
+  //$('#toc').addClass('navbar navbar-light bg-light')
+  $('#toc > ul').addClass('nav');
+  $('#toc > ul').find('li').addClass('nav-item');
+  $('#toc > ul').find('a').addClass('nav-link');
 });
